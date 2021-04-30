7a4f94a8e836a186a75cafa603c7fa1ba24bf2dd
YuriShido <sydonce57@gmail.com>
2021-04-30 15:13:41 -0700

    fixed issue

--- src/component/MemoContents.jsx
+++ src/component/MemoContents.jsx
@@ -1,3 +1,125 @@
+
+
+
+// import React, {useState,useEffect} from 'react'
+// import { connect } from 'react-redux'
+// import { addContents, setContents, updateContents} from '../redux/actions';
+
+// import { makeStyles } from '@material-ui/core/styles';
+// import TextField from '@material-ui/core/TextField';
+// import { MemoryRouter } from 'react-router';
+// import { CropSquareRounded, Update } from '@material-ui/icons';
+// import Button from '@material-ui/core/Button';
+
+
+
+
+// const MemoContents = ({contentsProps, addContents, setContents, currentProps, isNewProps, updateContents}) => {
+//     // メモの内容が変わった時にstateのリストに追加と更新をする
+//     // デフォルトは空欄かひとつ目のメモの内容が出るようにする
+//     const [title, setTitle] = useState('')
+//     const [memo, setMemo ]  = useState('')
+//     const [current, setCurrent] = useState(currentProps)
+
+//     console.log("inNewProps:", isNewProps);
+
+//     useEffect(() => {
+//         setCurrent(currentProps)
+//         console.log(currentProps);
+//         console.log("InuseEffect", current);
+//     }, [currentProps])
+
+//     const handleTitleChange = (e) => {
+//         e.preventDefault()
+//         setTitle(e.target.value)
+//         console.log('title: ', title);
+//     }
+
+//     // const handleMemoChange = (e) => {
+//     //     e.preventDefault()
+//     //     return {
+//     //         ...pre
+//     //     }
+//     //     setMemo(e.target.value)
+//     //     console.log("memo:", memo);
+
+//     // }
+//     const handleMemoChange = (e) => {
+//         setCurrent((prevProps) => {
+//             return {
+//                 ...prevProps,
+//                 contents: e.target.value
+//             }
+//         })
+//     }
+
+//     const clickHandler = (title, memo, isAdd, id=null) => {
+//         if(isAdd) {
+//             addContents({id: contentsProps.length, title: current.title, contents: current.contents})
+//         } else {
+//             console.log("updatetitle",title);
+//             console.log("updacontent",memo);
+
+//             updateContents({title: title, contents: memo, id: id})
+//         }
+//     }
+
+//     return (
+//         <>
+//         <form >
+//         <TextField onChange={handleTitleChange} className="title" id="standard-basic"  value={current.title} onChange={(e)=> setCurrent((prevProps) => {
+//             return {
+//                 ...prevProps,
+//                 title: e.target.value
+//             }
+//         })} />
+//         {/* <input onChange={handleTitleChange} className="title" id="standard-basic"  value={current.title}></input> */}
+//         <textarea className="memo-field" onChange={handleMemoChange} name="" id="" cols="30" rows="30"  defaultValue={current.contents}></textarea>
+//         <Button variant="contained" color="primary" onClick={ ()=>clickHandler(title, memo, true)}>Add</Button>
+//         <Button variant="outlined" color="primary" className="button" onClick={ ()=>clickHandler(current.title,current.contents, false, currentProps.id)}>Update</Button>
+//         {/* { isNewProps ?
+//         (<Button variant="contained" color="primary" onClick={ ()=>clickHandler(title, memo, true)}>Add</Button>) :
+//         (<Button variant="contained" color="primary" onClick={ ()=>clickHandler(title,memo, false)}>Update</Button>)
+//         } */}
+//         {/* { isNewProps ? (<button onClick={ ()=>clickHandler(title, memo, true)}>Add</button>):(<button onClick={ ()=>clickHandler(title,memo, false)}>Update</button>)} */}
+             
+        
+//       </form>
+
+//          {/* <div className="memo-box">
+//              <input className="memo-title" type="text"/>
+//              <textarea className="memo-field" name="" id="" cols="90" rows="30"></textarea>
+
+//          </div> */}
+//         </>
+//     )
+// }
+
+// const mapStateToProps = (state) => ({
+//     contentsProps: state.contents.memoList,
+//     currentProps: state.contents.currentMemo,
+//     isNewProps: state.contents.isNew
+    
+// })
+
+// const mapDispatchToProps = (dispatch) => {
+//     // console.log(addContents);
+//     return ({
+//     addContents: (contents) => dispatch(addContents(contents)),
+//     setContents: (contents) => dispatch(setContents(contents)),
+//     updateContents: (contents) => dispatch(updateContents(contents))
+// })}
+
+// export default connect(mapStateToProps, mapDispatchToProps)(MemoContents)
+
+// // const mapDispatchToProps = (dispatch) => ({
+// //     addItem: (item) => dispatch(addItem(item)),
+// //     removeItem: (item) => dispatch(removeItem(item)),
+// //     clearItem: (item) => dispatch(clearItemFromCart(item))
+// // })
+
+// // export default connect(null, mapDispatchToProps)(CheckOutItem)
+
 import React, {useState,useEffect} from 'react'
 import { connect } from 'react-redux'
 import { addContents, setContents, updateContents} from '../redux/actions';
@@ -71,7 +193,7 @@ const MemoContents = ({contentsProps, addContents, setContents, currentProps, is
             }
         })} />
         {/* <input onChange={handleTitleChange} className="title" id="standard-basic"  value={current.title}></input> */}
-        <textarea className="memo-field" onChange={handleMemoChange} name="" id="" cols="30" rows="30"  defaultValue={current.contents}></textarea>
+        <textarea className="memo-field" onChange={handleMemoChange} name="" id="" cols="30" rows="30"  value={current.contents}></textarea>
         <Button variant="contained" color="primary" onClick={ ()=>clickHandler(title, memo, true)}>Add</Button>
         <Button variant="outlined" color="primary" className="button" onClick={ ()=>clickHandler(current.title,current.contents, false, currentProps.id)}>Update</Button>
         {/* { isNewProps ?
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -63,18 +63,25 @@ const ContentsReducer = (state = INITIAL_STATE, action) => {
         case UPDATE_CONTENTS:
 
         const newMemoList = state.memoList.map((memo) => {
-            console.log("Memo;",memo)
-            console.log("action", action.payload);
             if(memo.id === action.payload.id){
+                console.log("Memo;",memo) // --> {id: 1, title: "Check2", contents: "Difficult"}
+                console.log("action", action.payload);  // --> {title: "Check1", contents: "yoyoyoyoyoyoyoyorhfw9hrfpr", id: 1}contents: "yoyoyoyoyoyoyoyorhfw9hrfpr"id: 1title: "Check1"__proto__: Object
+
                 return{
                     ...memo,
-                    [action.payload]: action.payload
+                    // [action.payload]: action.payload
+                    ...action.payload
+                }
+            } else {
+                return {
+                    ...memo
                 }
             }
         })
 
         return {
             ...state,
+            // memoList: 
             memoList: newMemoList
         }
 
@@ -125,3 +132,131 @@ const ContentsReducer = (state = INITIAL_STATE, action) => {
 }
 
 export default ContentsReducer
+
+// import { ADD_CONTENTS, SET_CONTENTS, UPDATE_CONTENTS, DELETE_ITEM} from './actions'
+
+// const INITIAL_STATE = {
+//     memoList: [
+//         {
+//             id:0,
+//             title: "Test",
+//             contents: "料理レシピ",
+
+//         },
+//         {
+//             id:1,
+//             title: "Check2",
+//             contents: "Difficult",
+
+//         }
+//     ],
+//     currentMemo: {
+//         id: "",
+//         title: "",
+//         contents: ""
+//     },
+
+//     isNew: true
+// }
+// //action stateの中身をarrayに変える
+// console.log("INITIIAL",INITIAL_STATE);
+// const ContentsReducer = (state = INITIAL_STATE, action) => {
+//     console.log("HAHAHA ", state.currentMemo);
+//     switch (action.type) {
+//         case ADD_CONTENTS:
+//             // const newState = { ...state }
+//             // newState.memoList.push(action.payload)
+//             // console.log("STATE IN ADD: ", newState);
+//             console.log("payload: ", action.payload);
+//             // return {
+//             //     ...state,
+//             //     ...newState
+//             // }
+//             return {
+//                 ...state,
+//                 memoList: [...state.memoList, action.payload]
+//             }
+
+//         // ...state, 
+//         //     memoList: [...state.memoList, action.payload]
+//         //         {title: addContents(state.title),
+//         //         contents: addContents(state.contents)}]
+
+//         case SET_CONTENTS:
+
+//             return {
+//                 ...state,
+//                 currentMemo: {
+//                     id: action.payload.id,
+//                     title: action.payload.title,
+//                     contents: action.payload.contents
+//                 },
+//                 isNew: true
+                
+//             }
+        
+//         case UPDATE_CONTENTS:
+
+//         const newMemoList = state.memoList.map((memo) => {
+//             console.log("Memo;",memo)
+//             console.log("action", action.payload);
+//             if(memo.id === action.payload.id){
+//                 return{
+//                     ...memo,
+//                     [action.payload]: action.payload
+//                 }
+//             }
+//         })
+
+//         return {
+//             ...state,
+//             memoList: newMemoList
+//         }
+
+//         // const newState2 = {...state}
+//         // // newState2.memoList[action.payload.id].title = action.payload.title
+//         // // newState2.memoList[action.payload.id].contents = action.payload.contents
+
+//         // console.log("updateAction",state.memoList[action.payload.id]);
+//         // return {
+//         //     // ...state,
+//         //     // memoList: {
+//         //     //     title: action.payload.title,
+//         //     //     contents: action.payload.contents
+//         //     // },
+//         //     // isNew: false
+//         //     ...state,
+//         //     memoList: [{...state.memoList[action.payload.id],
+              
+//         //         title: action.payload.title,
+//         //         contents: action.payload.contents}],
+//         //         isNew: false
+//         //         // title: action.payload.title,
+//         //         // contents: action.payload.contents
+            
+
+            
+//         //     // ...newState2
+
+
+//         // }
+
+//         case DELETE_ITEM: 
+//             console.log(action.payload,
+//                 state.memoList);
+//             return {
+//                 ...state,
+//                 memoList: state.memoList.filter(memo => memo.id !== action.payload.id)
+//             }
+
+//         //     case CLEAR_FROM_CART:
+//         //     return {
+//         //     ...state,
+//         //     cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+//         // }
+//         default:
+//             return state;
+//     }
+// }
+
+// export default ContentsReducer
